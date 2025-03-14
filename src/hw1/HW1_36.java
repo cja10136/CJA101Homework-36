0f96b7db69282be22a1dff4f2276cca0a52f41b3
CJA101_36 <clearng7531@gmail.com>
2025-03-14 16:15:35 +0800

    first commit

---
+++ src/hw1/HW1_36.java
@@ -0,0 +1,41 @@
+package hw1; // 已完成
+
+public class HW1_36 {
+	public static void main(String[] args) {
+		//1.請設計一隻Java程式，計算12，6這兩個數值的和與積
+		System.out.println("第一題："+ (12 + 6));
+		
+		//2.請設計一隻Java程式，計算200顆蛋共是幾打幾顆？(一打為12顆)
+		System.out.println("第二題：" + 200/12 + "打，" + 200%12 + "顆");
+		
+		//3.請由程式算出256559秒為多少天、多少小時、多少分與多少秒
+		int time = 256559;
+		int second = time % 60;
+		time -= second; time /= 60; // 將time的單位改為分鐘
+		int minu = time % 60;
+		time -= minu; time /= 60; // 將time的單位改為小時
+		int hour = time % 24;
+		time -= hour; time /= 24; // 將time的單位改為天
+		System.out.println("第三題：" + time + "天" + hour + "小時" + minu + "分" + second + "秒");
+		
+		//4.請定義一個常數為3.1415(圓周率)，並計算半徑為5的圓面積與圓周長
+		final double PI = 3.1415;
+		System.out.printf("第四題：圓面積:%.4f，圓周長:%.4f\n", PI*Math.pow(5, 2), 2*PI*5);
+		
+		//5.某人在銀行存入150萬，銀行利率為2%，如果每年利息都繼續存入銀行，請用程式計算10年後，
+		//	本金加利息共有多少錢 (用複利計算，公式請自行google)
+		double deposit = 1500000;
+		deposit *= Math.pow(1.02, 10);
+		System.out.printf("第五題：%.2f元\n", deposit);
+		
+		//6.請寫一隻程式，利用System.out.println()印出以下三個運算式結果：
+		//	5 + 5
+		//	5 + ‘5’
+		//	5 + “5”
+		//	並請用註解各別說明答案的產生原因
+		System.out.println("第六題：");
+		System.out.println(5 + 5); // 10，整數相加5+5=10
+		System.out.println(5 + '5'); // 58，因字元'5'的Unicode以十進位表示是53
+		System.out.println(5 + "5"); // 55，因Java會把int的5轉成string，兩個string連續印出
+	}
+}
---
+++ src/hw2/HW2_36.java
@@ -0,0 +1,117 @@
+package hw2;
+
+public class HW2_36 {
+	public static void main(String[] args) {
+		//1. 請設計一隻Java程式,計算1~1000的偶數和 (2+4+6+8+...+1000)
+		int result1 = 0;
+		for (int i = 2; i <= 1000; i += 2) {
+			result1 += i;
+		}
+		System.out.println("第一題：總和 " + result1);
+		
+		//2. 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用for迴圈)
+		int result2 = 1;
+		for (int i = 1; i <= 10; i ++) {
+			result2 *= i;
+		}
+		System.out.println("第二題：乘積 " + result2);
+		
+		//3. 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用while迴圈)
+		int i = 1, result3 = 1;
+		while (i <=10) {
+			result3 *= i;
+			i +=1;
+		}
+		System.out.println("第三題：乘積 " + result3);
+		
+		//4. 請設計一隻Java程式,輸出結果為以下:1 4 9 16 25 36 49 64 81 100
+		System.out.print("第四題：");
+		for (i = 1; i <= 10; i++) {
+			System.out.print((int)Math.pow(i, 2) + " ");
+		}
+		System.out.println();
+		
+		//5. 阿文很熱衷大樂透 (1 ~ 49),但他不喜歡有4的數字,不論是個位數或是十位數。請設計一隻程式,
+		//   輸出結果為阿文可以選擇的數字有哪些?總共有幾個?
+		int count = 0;
+		System.out.print("第五題：");
+		for (i = 1; i < 50; i++) {
+			if(i%10 == 4 || i/10 == 4)
+				continue;
+			System.out.print(i + " ");
+			count += 1;
+		}
+		System.out.printf("\n，總共%d個\n", count);
+		
+		//6. 請設計一隻Java程式,輸出結果為以下:
+		//	 1 2 3 4 5 6 7 8 9 10
+		//	 1 2 3 4 5 6 7 8 9
+		//	 1 2 3 4 5 6 7 8
+		// 	 1 2 3 4 5 6 7
+		//	 1 2 3 4 5 6
+		//	 1 2 3 4 5
+		//	 1 2 3 4
+		//	 1 2 3
+		//	 1 2
+		//	 1
+		System.out.println("第六題：");
+		for (i = 10; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		
+		//7. 請設計一隻Java程式,輸出結果為以下:
+		//	 A
+		//	 BB
+		//	 CCC
+		//	 DDDD
+		//	 EEEEE
+		//	 FFFFFF
+		char c = 'A';
+		System.out.println("第七題：");
+		for (i = 1; i <= 6; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(c);
+			}
+			c ++;
+			System.out.println();
+		}
+		
+		//8. 請建立一個TestNineNine.java程式,可輸出九九乘法表
+		System.out.println("第八題：");
+		//一:使用for迴圈 + while迴圈
+		System.out.println("使用for迴圈 + while迴圈：");
+		for (i = 1; i <=9 ; i++) {
+			int j = 1;
+			while (j <= 9) {
+				System.out.printf("%d*%d=%d ", i, j, i*j);
+				j++;
+			}
+			System.out.println();
+		}
+		//二:
+		System.out.println("\n使用for迴圈 + do while迴圈：");
+		for (i = 1; i <=9 ; i++) {
+			int j = 1;
+			do {
+				System.out.printf("%d*%d=%d ", i, j, i*j);
+				j++;
+			} while (j <= 9);
+			System.out.println();
+		}
+		//三:使用while迴圈 + do while迴圈
+		System.out.println("\n使用while迴圈 + do while迴圈：");
+		i = 1;
+		while (i <= 9) {
+			int j = 1;
+			do {
+				System.out.printf("%d*%d=%d ", i, j, i*j);
+				j++;
+			} while (j <= 9);
+			System.out.println();
+			i++;
+		}
+	}
+}
